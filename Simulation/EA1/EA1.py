56989aeb1487341390e97af09075720f2400f328
Kilian Ernst <kilianernst96@gmail.com>
2024-07-28 20:11:56 +0200

    Simulation: add solution EA1

---
+++ Simulation/EA1/EA1.py
@@ -0,0 +1,20 @@
+import numpy as np
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+from math import sin, cos
+
+
+
+def task_one():
+    pass
+
+def task_two():
+    pass
+
+def task_three():
+    pass
+
+if __name__ == "__main__":
+    task_one()
+    task_two()
+    task_three()
